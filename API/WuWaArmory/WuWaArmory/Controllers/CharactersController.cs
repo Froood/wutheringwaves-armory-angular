85c8a1b49cd4d30883fa6feccff43e3ecbb4967b
Frood <paybackamvz152@gmail.com>
2024-07-18 19:37:40 +0500

    Project Initialisation
    ASP .net core setup + Angular Project Created

---
+++ API/WuWaArmory/WuWaArmory/Controllers/CharactersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WuWaArmory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+       
+    }
+}
---
+++ API/WuWaArmory/WuWaArmory/Models/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace WuWaArmory.Models
+{
+    public class Character
+    {
+        public Guid Id { get; set; }
+        public int? Age { get; set; }
+        public required int Rating { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Region { get; set; }
+
+    }
+}
